253ea5b6604edca6e106df3ca604795061f3d4ce
drako <dany.rakotopara@ledger.fr>
2019-10-23 22:35:31 +0200

    sakamin

--- cosmos-synchronizer/synchronizer.js
+++ cosmos-synchronizer/synchronizer.js
@@ -2,17 +2,53 @@ var request = require('request');
 var sha256 = require('js-sha256');
 var kafka = require('kafka-node'),
     Consumer = kafka.Consumer,
+    ConsumerGroup = kafka.ConsumerGroup,
     HighLevelProducer = kafka.HighLevelProducer,
-    client = new kafka.KafkaClient(),
-    cosmos_consumer = new Consumer(
-        client,
-        [
-            { topic: 'cosmos'}
-        ],
-        {
-            autoCommit: true
-        }
-    );
+    client = new kafka.KafkaClient();
+
+    // cosmos_consumer = new Consumer(
+    //     client,
+    //     [
+    //         { topic: 'cosmos'}
+    //     ],
+    //     {
+    //         groupId: 'cosmos-synchronizer',
+    //         autoCommit: true,
+    //         autoCommitIntervalMs: 100,
+    //         fromOffset: false
+    //     }
+    // );
+
+
+var options = {
+    kafkaHost: 'localhost:9092', // connect directly to kafka broker (instantiates a KafkaClient)
+    batch: undefined, // put client batch settings if you need them
+    ssl: false, // optional (defaults to false) or tls options hash
+    groupId: 'cosmos-synchronizer',
+    sessionTimeout: 1500000,
+    autoCommit: true,
+    // An array of partition assignment protocols ordered by preference.
+    // 'roundrobin' or 'range' string for built ins (see below to pass in custom assignment protocol)
+    protocol: ['roundrobin'],
+    encoding: 'utf8', // default is utf8, use 'buffer' for binary data
+
+    // Offsets to use for new groups other options could be 'earliest' or 'none' (none will emit an error if no offsets were saved)
+    // equivalent to Java client's auto.offset.reset
+    fromOffset: 'latest', // default
+    commitOffsetsOnFirstJoin: true, // on the very first time this consumer group subscribes to a topic, record the offset returned in fromOffset (latest/earliest)
+    // how to recover from OutOfRangeOffset error (where save offset is past server retention) accepts same value as fromOffset
+    outOfRangeOffset: 'earliest', // default
+    fetchMaxBytes: 150000000
+    // Callback to allow consumers with autoCommit false a chance to commit before a rebalance finishes
+    // isAlreadyMember will be false on the first connection, and true on rebalances triggered after that
+    // onRebalance: (isAlreadyMember, callback) => { callback(); } // or null
+};
+
+var cosmos_consumer = new ConsumerGroup(options, 'cosmos');
+
+
+
+
 
 producer = new HighLevelProducer(client);
 const MSG_SYNC_TYPE = 'SYNC'
@@ -61,6 +97,7 @@ function get_tx(height, transactions) {
 }
 
 cosmos_consumer.on('message', function (message) {
+    // console.log(cosmos_consumer)
     // msg interface {"type": "SYNC", "address":"cosmos1sd4tl9aljmmezzudugs7zlaya7pg2895tyn79r", "height":2291227}
     // msg interface {"type": "SYNC", "address":"cosmos1s43l7s99hx627scn2ldd3ey50qpj9frv89zkyv"}
     try {
@@ -106,4 +143,8 @@ cosmos_consumer.on('message', function (message) {
     } catch(error){
         console.error('wrong json for message', message.value);
     }
+});
+
+cosmos_consumer.on('connect', function (message) {
+    console.log('connect')
 });
--- sakamin-db/sakamin_db.js
+++ sakamin-db/sakamin_db.js
@@ -12,7 +12,7 @@ var kafka = require('kafka-node'),
     );
 
 const MSG_UPDATE_TX_WALLET_TYPE = 'UPDATE_TX_WALLET';
-
+const MongoClient = require('mongodb').MongoClient;
 
 cosmos_consumer.on('message', function (message) {
     try {
@@ -20,10 +20,22 @@ cosmos_consumer.on('message', function (message) {
         // {"type":"UPDATE_TX_WALLET","address":"cosmos1sd4tl9aljmmezzudugs7zlaya7pg2895tyn79r","transactions":
         if (jsonMsg.type.localeCompare(MSG_UPDATE_TX_WALLET_TYPE) === 0) {
             let address = jsonMsg.address;
-            let transaction = jsonMsg.transactions;
-            console.log('[Cosmos][Sakamin-db][Update TX] for', address)
+            let transactions = jsonMsg.transactions;
+            console.log('[Cosmos][Sakamin-db][Update TX] for', address);
+
+            MongoClient.connect('mongodb://localhost:27017', function (err, client) {
+                if (err) throw err;
+                const db = client.db('sakamin');
+                db.collection('Transactions').updateOne(
+                    {address},
+                    { $push: { transactions: {$each:transactions} }},
+                    // { $set: { transactions: { $concatArrays: [ "$transactions", transactions ] } } },
+                    { upsert: true }
+                );
+            });
         }
     } catch(error){
         console.error('wrong json for message', message.value);
+        console.log(error)
     }
 });
